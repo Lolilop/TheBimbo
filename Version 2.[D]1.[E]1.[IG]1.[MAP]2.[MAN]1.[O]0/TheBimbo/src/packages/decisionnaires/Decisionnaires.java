c0fb599afe06f52a5bb55d59f7d2233f2cb11328
lazerstory <theo.gerfaux@orange.fr>
2019-04-04 21:05:43 +0200

    Add files via upload

---
+++ Version 2.[D]1.[E]1.[IG]1.[MAP]2.[MAN]1.[O]0/TheBimbo/src/packages/decisionnaires/Decisionnaires.java
@@ -0,0 +1,140 @@
+package packages.decisionnaires;
+
+import packages.etat.Etat;
+import packages.map.Grille;
+
+/**
+ * Création des différents decisionnaires (joueur et IA)
+ * @author TTurq
+ * version 1.0
+ */
+public abstract class Decisionnaires {
+
+    private short posX;
+    private short posY;
+
+    /**
+     * Construction d'un decisionnaires
+     * @param x position x
+     * @param y position y
+     * @param grille grille
+     * @param etat etat
+     */
+
+    public Decisionnaires(final short x, final short y, final Grille grille, final Etat etat) {
+
+        this.posX = x;
+        this.posY = y;
+        grille.setPosition(posX, posY, etat);
+    }
+
+    /**
+     * retourne la position x
+     * @return position x
+     */
+
+    public short getX(){
+        return this.posX;
+    }
+
+    /**
+     * return la position y
+     * @return position y
+     */
+
+    public short getY(){
+        return this.posY;
+    }
+
+    /**
+     * change la position x
+     * @param x position x
+     */
+
+    public void setX(short x){
+        this.posX += x;
+    }
+
+    /**
+     * change la position y
+     * @param y position y
+     */
+
+    public void setY(short y){
+        this.posY += y;
+    }
+
+    /**
+     * permet au joueur de se deplacer au Nord
+     * @param grille
+     */
+
+    public abstract void directionNord(final Grille grille);
+
+    /**
+     * permet au joueur de se deplacer au Sud
+     * @param grille
+     */
+
+    public abstract void directionSud(final Grille grille);
+
+    /**
+     * permet au joueur de se deplacer à l'Est
+     * @param grille
+     */
+
+    public abstract void directionEst(final Grille grille);
+
+    /**
+     * permet au joueur de se deplacer à l'Ouest
+     * @param grille
+     */
+
+    public abstract void directionOuest(final Grille grille);
+
+    /**
+     * verifie la possibilite d'aller au Nord
+     * @return vrai si c'est bon
+     */
+
+    public boolean continuerNord() {
+        return((getY() - 1) > -1);
+    }
+
+    /**
+     * verifie la possibilite d'aller au Sud
+     * @return vrai si c'est bon
+     */
+
+    public boolean continuerSud() {
+        return((getY() + 1) < Grille.getTailleY());
+    }
+
+    /**
+     * verifie la possibilite d'aller a l'Est
+     * @return vrai si c'est bon
+     */
+
+    public boolean continuerEst() {
+        return((getX() + 1 < Grille.getTailleX()));
+    }
+
+    /**
+     * verifie la possibilite d'aller a l'Ouest
+     * @return vrai si c'est bon
+     */
+
+    public boolean continuerOuest() {
+        return((getX() - 1) > -1);
+    }
+
+    /**
+     * Verifie si le joueur ou l'ia a gagner
+     * @param grille
+     * @param y
+     * @param x
+     * @return vrai si l'un des deux a gagner
+     */
+    
+    public abstract boolean deplacer(final Grille grille, String direction); //direction uniquement pour le joueur, String quelconque pour les ia
+}
