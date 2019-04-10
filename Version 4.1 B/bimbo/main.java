0fc208fde0a4b7d0015169629b5c776494faa50b
Lolilop <T.turquin@outlook.fr>
2019-04-10 15:16:47 +0200

    Add files via upload

---
+++ Version 4.1 B/bimbo/main.java
@@ -0,0 +1,31 @@
+package bimbo;
+
+
+import java.io.IOException;
+import static java.lang.Thread.sleep;
+
+
+
+
+import packages.interfaces.Interface;
+import packages.outils.chrono.Chrono;
+
+
+/**
+ *
+ * @author TTurq
+ */
+
+public class main {
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        
+
+         Interface ihm = new Interface();
+        ihm.setVisible(true);
+
+    }
+    
+   
+    
+}
